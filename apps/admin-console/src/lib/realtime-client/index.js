02387198870364c336485cb1657b46e13bbac7f7
Thousand Technology Web Services <thousandtws@gmail.com>
2026-02-18 06:45:29 -0300

    corrige caminhos de importação e adiciona package-lock.json

--- apps/admin-console/src/lib/realtime-client/index.js
+++ apps/admin-console/src/lib/realtime-client/index.js
@@ -1,11 +1,11 @@
 "use client";
 
-export { useRealtimeChannel as default, useRealtimeChannel } from "./src/use-realtime-channel.js";
-export { NotificationProvider, useNotificationBus } from "./src/notification-bus.js";
-export { REALTIME_CHANNELS, REALTIME_EVENT_TYPES } from "./src/constants.js";
+export { useRealtimeChannel as default, useRealtimeChannel } from "./use-realtime-channel.js";
+export { NotificationProvider, useNotificationBus } from "./notification-bus.js";
+export { REALTIME_CHANNELS, REALTIME_EVENT_TYPES } from "./constants.js";
 export {
   parseBridgeEvent,
   dispatchBridgeEvent,
   createProposalDraftSnapshot,
   buildNotificationPayload,
-} from "./src/utils.js";
+} from "./utils.js";
