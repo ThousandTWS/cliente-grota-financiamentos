f7fd578e2a300e792a4cc82d98cfd469d72f6b72
ThousandTWS Engineering <vinicius@thousandtws.com>
2026-04-08 09:46:06 -0300

    fiz

--- apps/admin-console/app/(auth)/verificacao-token/VerifyTokenPageWrapper.tsx
+++ apps/admin-console/app/(auth)/verificacao-token/VerifyTokenPageWrapper.tsx
@@ -3,6 +3,5 @@
 import dynamic from 'next/dynamic';
 
 export const VerifyTokenPageWrapper = dynamic(
-  () => import('@/presentation/features/auth/components/verify-token-page').then((mod) => mod.VerifyTokenPage),
-  { ssr: false }
+  () => import('@/presentation/features/auth/components/verify-token-page').then((mod) => mod.VerifyTokenPage)
 );
