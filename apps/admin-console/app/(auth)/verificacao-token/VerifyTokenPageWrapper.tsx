593e771577d8a1758ffb8dfd8b4ebf6b5cea8b75
ThousandTWS Engineering <vinicius@thousandtws.com>
2026-04-08 10:24:59 -0300

    refactor: remove VerifyTokenPageWrapper and related components

--- apps/admin-console/app/(auth)/verificacao-token/VerifyTokenPageWrapper.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import dynamic from 'next/dynamic';
-
-export const VerifyTokenPageWrapper = dynamic(
-  () => import('@/presentation/features/auth/components/verify-token-page').then((mod) => mod.VerifyTokenPage)
-);
